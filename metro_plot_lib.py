f7480903197da5612bcc963616ca72bee3ffbc29
Calvin Fai <cfai2304@gmail.com>
2022-11-03 19:58:44 -0400

    matplotlib miscellany

--- metro_plot_lib.py
+++ metro_plot_lib.py
@@ -9,19 +9,22 @@ from matplotlib.cm import ScalarMappable
 import numpy as np
 from math import floor, ceil
 
-def make_1D_tracker(all_proposed, all_accepted, **kwargs):
+def make_1D_tracker(x, all_accepted, **kwargs):
     mark_value = kwargs.get("mark_value", None)
     size = kwargs.get("size", (3.5,2.7))
     ylabel = kwargs.get("ylabel", "")
     xlabel = kwargs.get("xlabel", "Sample #")
     do_log = kwargs.get("do_log", 0)
     show_legend = kwargs.get("show_legend", 0)
-    burn = kwargs.get("burn", 0)
-    did_multicore = kwargs.get("did_multicore", (all_proposed.ndim == 2))
+    did_multicore = kwargs.get("did_multicore", (x.ndim == 2))
     
-    fig, ax = plt.subplots(1,1,dpi=200, figsize=size)
+    fig = kwargs.get("fig", None)
+    ax = kwargs.get("ax", None)
+    
+    if fig is None or ax is None:
+        fig, ax = plt.subplots(1,1,dpi=200, figsize=size)
     if did_multicore:
-        for i in range(len(all_proposed)):
+        for i in range(len(x)):
             if i == 0:
                 labelp = "Proposed"
                 labela = "Accepted"
@@ -29,16 +32,15 @@ def make_1D_tracker(all_proposed, all_accepted, **kwargs):
                 labelp = None
                 labela = None
             
-            ax.plot(all_proposed[i], label=labelp, color='steelblue', zorder=1)
-            ax.plot(all_accepted[i], label=labela, color='orange', zorder=2)
+            #ax.plot(all_proposed[i], label=labelp, color='steelblue', zorder=1)
+            ax.plot(x[i], label=labela, color='orange', zorder=2)
     
     else:
-        ax.plot(all_proposed, label="Proposed samples", color='steelblue', zorder=1)
-        ax.plot(all_accepted, label="Accepted samples", color='orange', zorder=2)
+        #ax.plot(all_proposed, label="Proposed samples", color='steelblue', zorder=1)
+        ax.plot(x, all_accepted, label="Accepted samples", color='orange', zorder=2)
     if mark_value is not None:
         ax.axhline(mark_value, color='r', linestyle='dashed', label="Actual value", zorder=3)
     
-    ax.axvline(burn, color='k', linestyle='dashed', linewidth=1)
     ax.set_ylabel(ylabel)
     ax.set_xlabel(xlabel)
     if do_log: ax.set_yscale('log')
@@ -53,6 +55,7 @@ def make_1D_histo(accepted, **kwargs):
     do_log = kwargs.get("do_log", 0)
     bin_count = kwargs.get("bin_count", 96)
     did_multicore = kwargs.get("did_multicore", (accepted.ndim == 2))
+    axis_overrides = kwargs.get("axis_overrides", None)
     
     minx = min(accepted.flatten())
     maxx = max(accepted.flatten())
@@ -61,7 +64,8 @@ def make_1D_histo(accepted, **kwargs):
         accepted = np.log10(accepted)
         minx = np.log10(minx)
         maxx = np.log10(maxx)
-        mark_value = np.log10(mark_value)
+        if mark_value is not None:
+            mark_value = np.log10(mark_value)
     
     bins_x = np.arange(bin_count+1)
 
@@ -80,11 +84,15 @@ def make_1D_histo(accepted, **kwargs):
         if len(decades) == 1:
             decades = np.arange(decades[0] - 1, decades[-1] + 2)
         ax.set_xticks(decades)
+        ax.set_xticks(np.concatenate([d + np.log10(np.linspace(1,10,10)) for d in decades]), minor=True)
         ax.set_xlim((decades[0], decades[-1]))
         ax.set_xticklabels([r"$10^{{{}}}$".format(d) for d in decades])
     
     ax.set_ylabel(f"P({xlabel})")
     ax.set_xlabel(xlabel)
+    if axis_overrides is not None:
+        ax.set_xlim(*axis_overrides[xlabel])
+    
     fig.tight_layout()
     
 def make_2D_histo(x_accepted, y_accepted, **kwargs):
@@ -177,29 +185,45 @@ def make_2D_tracker(x_accepted, y_accepted, **kwargs):
     markx = kwargs.get("markx", None)
     marky = kwargs.get("marky", None)
     did_multicore = kwargs.get("did_multicore", (x_accepted.ndim == 2))
+    axis_overrides = kwargs.get("axis_overrides", None)
     
-    fig, ax2d = plt.subplots(1,1,figsize=(3.5*1.1,3.5), dpi=120)
+    fig = kwargs.get("fig", None)
+    ax2d = kwargs.get("ax", None)
+    first = kwargs.get('first', True)
     
-    
+    if fig is None or ax2d is None:
+        fig, ax2d = plt.subplots(1,1,figsize=(3.5*1.1,3.5), dpi=120)
     
     if did_multicore:
         
         for i in range(len(x_accepted)):
             color_grad = np.linspace(0, 1, len(x_accepted[i]))
-            ax2d.scatter(x_accepted[i], y_accepted[i], c=color_grad)
+            ax2d.plot(x_accepted[i], y_accepted[i], c=color_grad)
     else:
         color_grad = np.linspace(0, 1, len(x_accepted))
-        ax2d.scatter(x_accepted, y_accepted, c=color_grad)
+        ax2d.plot(x_accepted, y_accepted, zorder=1, color='orange')
+        ax2d.scatter(x_accepted[0], y_accepted[0], c='purple', zorder=99)
+        ax2d.scatter(x_accepted[-1], y_accepted[-1], c='red', marker='s', zorder=99)
+        
+    # if first:
+    #     def fmt(s):
+    #         return r"$10^{{{:.0f}}}$".format(s)
+    #     xyz = np.load("tempx.npy")
+    #     Y_corr, X_corr, marP = xyz
+    #     im_corr = ax2d.contour(10 ** Y_corr, 10 ** X_corr, marP, levels=np.arange(0,7,1), colors='k', zorder=0)
+    #     #ax2d.clabel(im_corr, im_corr.levels, inline=False, fmt=fmt,fontsize=10, zorder=0)
+    
+        
     if markx is not None and marky is not None:
-        ax2d.scatter(markx, marky, c='r', marker='s')
+        ax2d.scatter(markx, marky, c='r', marker='*', zorder=99)
 
     if contour_info is not None:
         cx = contour_info[0]
         cy = contour_info[1]
         cZ = contour_info[2]
         clevels = contour_info[3]
-        cwg = ax2d.contour(cx, cy, cZ, levels=clevels, colors='black', zorder=9999)
-        ax2d.clabel(cwg)        
+        cwg = ax2d.contour(cx, cy, cZ, levels=clevels, colors='black', zorder=0)
+        #ax2d.clabel(cwg)        
 
     ax2d.set_xlabel(xlabel)
     ax2d.set_ylabel(ylabel)
@@ -207,6 +231,26 @@ def make_2D_tracker(x_accepted, y_accepted, **kwargs):
         ax2d.set_yscale('log')
     if logx:
         ax2d.set_xscale('log')
-    cbar = fig.colorbar(ScalarMappable(), ax=ax2d, ticks=[0,1])
-    cbar.ax.set_yticklabels(["Initial", "Final"])
+        
+    if axis_overrides is not None:
+        ax2d.set_ylim(axis_overrides[1])
+        ax2d.set_xlim(axis_overrides[0])
+        
+    # if first:
+    #     cbar = fig.colorbar(ScalarMappable(), ax=ax2d, ticks=[0,1])
+    #     cbar.ax.set_yticklabels(["Initial", "Final"])
     fig.tight_layout()
+    fig.savefig(f"sfsb.png", transparent=True)
+    
+def plot_history(x, y, **kwargs):
+    """ Plot a simple 1D curve with data (x,y) """
+    title = kwargs.get("title", "")
+    xlabel = kwargs.get("xlabel", "")
+    ylabel = kwargs.get("ylabel", "")
+    
+    fig, ax = plt.subplots(1,1,figsize=(4,3), dpi=120)
+    ax.plot(x,y)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    ax.set_title(title)
+    
