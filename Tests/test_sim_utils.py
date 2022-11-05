b8e8e339cc6fd267818bc4c53a969ff7c929f741
Calvin Fai <cfai2304@gmail.com>
2022-11-04 23:50:58 -0400

    Tests for Solution() objects

--- Tests/test_sim_utils.py
+++ Tests/test_sim_utils.py
@@ -4,42 +4,7 @@ import sys
 sys.path.append("..")
 from sim_utils import Parameters, Solution, Grid, Covariance, MetroState
 
-class TestUtils(unittest.TestCase):
-           
-    def test_Solution(self):
-        testS = Solution()
-        testG = Grid()
-        testG.dx = 1
-        
-        num_nodes = 128
-        num_tsteps = 10
-        testS.N = np.ones((num_tsteps, num_nodes))
-        testS.P = np.ones((num_tsteps, num_nodes))
-        
-        # No PL
-        param_info = {'names':["ks", 'n0', 'p0'],
-                      'active':{"ks":1, 'n0':1, 'p0':1}}
-        initial_guesses = {"ks":0, 'n0':0, 'p0':0}
-        
-        testP = Parameters(param_info, initial_guesses)
-        testS.calculate_PL(testG, testP)
-        np.testing.assert_equal(np.zeros(num_tsteps), testS.PL)
-        
-        # Some PL
-        initial_guesses = {"ks":1, 'n0':0, 'p0':0}
-        testS.P = np.ones((num_tsteps, num_nodes)) * 10
-        testP = Parameters(param_info, initial_guesses)
-        testS.calculate_PL(testG, testP)
-        np.testing.assert_equal(np.ones(num_tsteps) * 10 * num_nodes, testS.PL)
-        
-        # Some more PL
-        initial_guesses = {"ks":2, 'n0':1, 'p0':1}
-        testS.P = np.ones((num_tsteps, num_nodes)) * 10
-        testP = Parameters(param_info, initial_guesses)
-        testS.calculate_PL(testG, testP)
-        np.testing.assert_equal(np.ones(num_tsteps) * 18 * num_nodes, testS.PL)
-        
-        return
+class TestUtils(unittest.TestCase):       
     
     def test_MetroState(self):
         # Will only look for these
@@ -83,7 +48,3 @@ class TestUtils(unittest.TestCase):
         
         # Descaled Covariance matrix
         np.testing.assert_equal(ms.variances.big_sigma, np.eye(4))
-        
-if __name__ == "__main__":
-    t = TestUtils()
-    t.test_History()
---
+++ Tests/test_sim_utils_Solution.py
@@ -0,0 +1,57 @@
+import unittest
+import numpy as np
+import sys
+sys.path.append("..")
+from sim_utils import Parameters, Solution, Grid
+
+class TestUtils(unittest.TestCase):
+           
+    def test_Solution(self):
+        testS = Solution()
+        testG = Grid()
+        testG.dx = 1
+        
+        num_nodes = 128
+        num_tsteps = 10
+        testS.N = np.ones((num_tsteps, num_nodes))
+        testS.P = np.ones((num_tsteps, num_nodes))
+        
+        # No PL
+        param_info = {'names':["ks", 'n0', 'p0'],
+                      'active':{"ks":1, 'n0':1, 'p0':1}}
+        initial_guesses = {"ks":0, 'n0':0, 'p0':0}
+        
+        testP = Parameters(param_info, initial_guesses)
+        testS.calculate_PL(testG, testP)
+        np.testing.assert_equal(np.zeros(num_tsteps), testS.PL)
+        
+        # Some PL
+        initial_guesses = {"ks":1, 'n0':0, 'p0':0}
+        testS.P = np.ones((num_tsteps, num_nodes)) * 10
+        testP = Parameters(param_info, initial_guesses)
+        testS.calculate_PL(testG, testP)
+        expected_PL = initial_guesses["ks"] * (testS.N * testS.P - initial_guesses["n0"]*initial_guesses["p0"])
+        # Like integrating, since dx=1
+        np.testing.assert_equal(expected_PL[:,0] * num_nodes, testS.PL)
+        
+        # Some more PL
+        initial_guesses = {"ks":2, 'n0':1, 'p0':1}
+        testS.P = np.ones((num_tsteps, num_nodes)) * 10
+        testP = Parameters(param_info, initial_guesses)
+        testS.calculate_PL(testG, testP)
+        expected_PL = initial_guesses["ks"] * (testS.N * testS.P - initial_guesses["n0"]*initial_guesses["p0"])
+        np.testing.assert_equal(expected_PL[:,0] * num_nodes, testS.PL)
+        
+        # Some TRTS
+        param_info = {'names':["mu_n", "mu_p", 'n0', 'p0'],
+                      'active':{"mu_n":1, "mu_p":1, 'n0':1, 'p0':1}}
+        initial_guesses = {"mu_n":1, "mu_p":1, 'n0':1, 'p0':1}
+        testS.P = np.ones((num_tsteps, num_nodes)) * 10
+        testP = Parameters(param_info, initial_guesses)
+        testS.calculate_TRTS(testG, testP)
+        expected_TRTS = (initial_guesses["mu_n"] * (testS.N - initial_guesses["n0"]) +
+                       initial_guesses["mu_p"] * (testS.P - initial_guesses["p0"]))
+        np.testing.assert_equal(expected_TRTS[:,0] * num_nodes, testS.trts)
+        
+        return
+    
