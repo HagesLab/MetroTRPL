5bc2dd2871dc24a1cb3211e70e6425af0fd15b69
Calvin Fai <cfai2304@gmail.com>
2022-12-31 23:22:12 -0500

    Add sim_flags to config file

--- bayes_io.py
+++ bayes_io.py
@@ -45,7 +45,6 @@ def get_data(exp_file, ic_flags, sim_flags, scale_f=1e-23, verbose=False):
     
     if NOISE_LEVEL is not None:
         y = (np.array(y) + NOISE_LEVEL*np.random.normal(0, 1, len(y)))
-
     else:
         y = np.array(y)
 
@@ -82,7 +81,6 @@ def get_data(exp_file, ic_flags, sim_flags, scale_f=1e-23, verbose=False):
         u_list[i] *= scale_f[i]
             
     if LOG_PL:
-
         # Deal with noisy negative values before taking log
         for i in range(len(t_list)):
             y_list[i] = np.abs(y_list[i])
@@ -133,6 +131,7 @@ def read_config_file(path):
         grid = [-1, -1, -1]
         param_info = {}
         meas_flags = {}
+        sim_flags = {}
 
         init_flag = 0
         
@@ -164,6 +163,9 @@ def read_config_file(path):
                 
             if "p$ Measurement handling flags" in line:
                 init_flag = 'm'
+                
+            if "p$ MCMC Control flags" in line:
+                init_flag = 's'
 
             if len(line_split) > 1:
 
@@ -219,29 +221,74 @@ def read_config_file(path):
                             meas_flags["noise_level"] = None
                         else:
                             meas_flags["noise_level"] = float(line_split[1])
+                            
+                if (init_flag == 's'):
+                    if line.startswith("Num iters"):
+                        sim_flags["num_iters"] = int(line_split[1])
+                    elif line.startswith("Solver name"):
+                        sim_flags["solver"] = line_split[1]
+                    elif line.startswith("Solver rtol"):
+                        sim_flags["rtol"] = float(line_split[1])
+                    elif line.startswith("Solver atol"):
+                        sim_flags["atol"] = float(line_split[1])
+                    elif line.startswith("Solver hmax"):
+                        sim_flags["hmax"] = float(line_split[1])
+                    elif line.startswith("Repeat hmax"):
+                        sim_flags["verify_hmax"] = bool(line_split[1])
+                    elif line.startswith("Anneal coefs"):
+                        sim_flags["anneal_params"] = extract_values(line_split[1], '\t')
+                    elif line.startswith("Force equal mu"):
+                        sim_flags["override_equal_mu"] = bool(line_split[1])
+                    elif line.startswith("Force equal S"):
+                        sim_flags["override_equal_s"] = bool(line_split[1])
+                    elif line.startswith("Use log of measurements"):
+                        sim_flags["log_pl"] = bool(line_split[1])
+                    elif line.startswith("Normalize all meas and sims"):
+                        sim_flags["self_normalize"] = bool(line_split[1])
+                    elif line.startswith("Proposal function"):
+                        sim_flags["proposal_function"] = line_split[1]
+                    elif line.startswith("Propose params one-at-a-time"):
+                        sim_flags["one_param_at_a_time"] = bool(line_split[1])
+                    elif line.startswith("Checkpoint dir"):
+                        sim_flags["checkpoint_dirname"] = os.path.join(line_split[1])
+                    elif line.startswith("Checkpoint fileheader"):
+                        sim_flags["checkpoint_header"] = line_split[1]
+                    elif line.startswith("Checkpoint freq"):
+                        sim_flags["checkpoint_freq"] = int(line_split[1])
+                    elif line.startswith("Load checkpoint"):
+                        if line_split[1] == "None":
+                            sim_flags["load_checkpoint"] = None
+                        else:
+                            sim_flags["checkpoint_dirname"] = line_split[1]
                         
-    return grid, param_info, meas_flags
+    return grid, param_info, meas_flags, sim_flags
 
 
-def generate_config_file(path, simPar, param_info, measurement_flags, verbose=False):
+def generate_config_file(path, simPar, param_info, measurement_flags, sim_flags, verbose=False):
     if isinstance(simPar[0], (float, int)):
         simPar[0] = [simPar[0]]
     
     with open(path, "w+") as ofstream:
         ofstream.write("$$ MCMC CONFIG CREATED {} AT {}\n".format(datetime.datetime.now().date(),
                                                                   datetime.datetime.now().time()))
+        ofstream.write("##\n")
         ofstream.write("p$ Space Grid:\n")
+        if verbose: ofstream.write("# List of material/system thicknesses - one per measurement\n")
         ofstream.write(f"Length(s): {simPar[0][0]}")
         for value in simPar[0][1:]:
             ofstream.write(f"\t{value}")
         ofstream.write('\n')
+        if verbose: ofstream.write("# Number of space nodes used by solver discretization\n")
         ofstream.write(f"nx: {simPar[1]}\n")
+        if verbose: ofstream.write("# Model to use to simulate each measurement\n")
         ofstream.write(f"Measurement type(s): {simPar[2][0]}")
         for value in simPar[2][1:]:
             ofstream.write(f"\t{value}")
         ofstream.write('\n')
         #######################################################################
+        ofstream.write("##\n")
         ofstream.write("p$ Param Info:\n")
+        if verbose: ofstream.write("# List of names of parameters used in the model\n")
         param_names = param_info["names"]
         ofstream.write(f"Param Names: {param_names[0]}")
         for value in param_names[1:]:
@@ -278,6 +325,7 @@ def generate_config_file(path, simPar, param_info, measurement_flags, verbose=Fa
             ofstream.write(f"\t{init_variance.get(name, 0)}")
         ofstream.write('\n')
         #######################################################################
+        ofstream.write("##\n")
         ofstream.write("p$ Measurement handling flags:\n")
         tc = measurement_flags["time_cutoff"]
         ofstream.write(f"Time cutoffs: {tc[0]}\t{tc[1]}\n")
@@ -291,11 +339,50 @@ def generate_config_file(path, simPar, param_info, measurement_flags, verbose=Fa
             ofstream.write("\n")
         noise_level = measurement_flags["noise_level"]
         ofstream.write(f"Added noise level: {noise_level}\n")
+        #######################################################################
+        ofstream.write("##\n")
+        ofstream.write("p$ MCMC Control flags:\n")
+        num_iters = sim_flags["num_iters"]
+        ofstream.write(f"Num iters: {num_iters}\n")
+        solver = sim_flags["solver"]
+        ofstream.write(f"Solver name: {solver}\n")
+        rtol = sim_flags["rtol"]
+        ofstream.write(f"Solver rtol: {rtol}\n")
+        atol = sim_flags["atol"]
+        ofstream.write(f"Solver atol: {atol}\n")
+        hmax = sim_flags["hmax"]
+        ofstream.write(f"Solver hmax: {hmax}\n")
+        verify_hmax = sim_flags["verify_hmax"]
+        ofstream.write(f"Repeat hmax: {verify_hmax}\n")
+        anneal = sim_flags["anneal_params"]
+        ofstream.write(f"Anneal coefs: {anneal[0]}\t{anneal[1]}\t{anneal[2]}\n")
+        emu = sim_flags["override_equal_mu"]
+        ofstream.write(f"Force equal mu: {emu}\n")
+        es = sim_flags["override_equal_s"]
+        ofstream.write(f"Force equal S: {es}\n")
+        logpl = sim_flags["log_pl"]
+        ofstream.write(f"Use log of measurements: {logpl}\n")
+        norm = sim_flags["self_normalize"]
+        ofstream.write(f"Normalize all meas and sims: {norm}\n")
+        prop_f = sim_flags["proposal_function"]
+        ofstream.write(f"Proposal function: {prop_f}\n")
+        gibbs = sim_flags["one_param_at_a_time"]
+        ofstream.write(f"Propose params one-at-a-time: {gibbs}\n")
+        chpt_d = sim_flags["checkpoint_dirname"]
+        ofstream.write(f"Checkpoint dir: {chpt_d}\n")
+        chpt_h = sim_flags["checkpoint_header"]
+        ofstream.write(f"Checkpoint fileheader: {chpt_h}\n")
+        chpt_f = sim_flags["checkpoint_freq"]
+        ofstream.write(f"Checkpoint freq: {chpt_f}\n")
+        load_chpt = sim_flags["load_checkpoint"]
+        ofstream.write(f"Load checkpoint: {load_chpt}\n")
         
     return
         
 if __name__ == "__main__":
-    grid, param_info, meas_flags = read_config_file("mcmc.txt")
+    grid, param_info, meas_flags, sim_flags = read_config_file("mcmc.txt")
     print(grid)
     print(param_info)
     print(meas_flags)
+    print(sim_flags)
+    print(os.listdir(sim_flags["checkpoint_dirname"]))
