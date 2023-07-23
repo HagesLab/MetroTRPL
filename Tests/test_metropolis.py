6a437835ebd2d0ef16ce1ba59f875f6370d27aa6
Calvin Fai <cfai2304@gmail.com>
2023-07-23 15:22:01 -0400

    Add normalization test case

--- Tests/test_metropolis.py
+++ Tests/test_metropolis.py
@@ -1,7 +1,9 @@
 import unittest
-import numpy as np
 import logging
 import sys
+sys.path.append("..")
+import numpy as np
+from scipy.integrate import trapz
 
 from metropolis import all_signal_handler
 from metropolis import E_field, model, select_next_params
@@ -10,8 +12,6 @@ from metropolis import detect_sim_fail, detect_sim_depleted, almost_equal
 from metropolis import check_approved_param
 from metropolis import run_iteration
 from sim_utils import Parameters, Grid, Covariance
-from scipy.integrate import trapz
-eps0 = 8.854 * 1e-12 * 1e-9  # [C / V m] to {C / V nm}
 q = 1.0  # [e]
 q_C = 1.602e-19  # [C]
 kB = 8.61773e-5  # [eV / K]
@@ -30,6 +30,7 @@ class TestUtils(unittest.TestCase):
         all_signal_handler(f)
 
     def test_E_field(self):
+        eps0 = 8.854 * 1e-12 * 1e-9  # [C / V m] to {C / V nm}
         # Test 1D
         vals = {'n0': 0,
                 'p0': 0,
@@ -646,7 +647,6 @@ class TestUtils(unittest.TestCase):
 
         sim_flags = {"current_sigma": 1,
                      "hmax": 4, "rtol": 1e-5, "atol": 1e-8,
-                     "measurement": "TRPL",
                      "self_normalize": None,
                      "solver": "solveivp", }
 
@@ -722,7 +722,6 @@ class TestUtils(unittest.TestCase):
 
         sim_flags = {"current_sigma": 1,
                      "hmax": 4, "rtol": 1e-5, "atol": 1e-8,
-                     "measurement": "TRPL",
                      "self_normalize": None,
                      "solver": "solveivp", }
 
@@ -743,6 +742,69 @@ class TestUtils(unittest.TestCase):
         # First iter; auto-accept
         np.testing.assert_almost_equal(
             p.likelihood, [-29701, -16309], decimal=0)  # rtol=1e-5
+
+    def test_run_iter_normalize(self):
+        # Same as test_run_iter, except self_normalization is active
+        # log(sol) should be much closer to vals (which is zero) as a result
+        np.random.seed(42)
+        Length = [2000, 2000]                            # Length (nm)
+        L = [2 ** 7, 2 ** 7]                                # Spatial point
+        mtype = ["TRPL", "TRPL"]
+        simPar = {"lengths": Length,
+                  "nx": L,
+                  "meas_types": mtype,
+                  "num_meas": 2}
+
+        iniPar = [1e15 * np.ones(L[0]) * 1e-21, 1e16 * np.ones(L[1]) * 1e-21]
+
+        param_names = ["n0", "p0", "mu_n", "mu_p", "ks", "Cn", "Cp", "Tm",
+                       "Sf", "Sb", "tauN", "tauP", "eps"]
+        unit_conversions = {"n0": ((1e-7) ** 3), "p0": ((1e-7) ** 3),
+                            "mu_n": ((1e7) ** 2) / (1e9), "mu_p": ((1e7) ** 2) / (1e9),
+                            "ks": ((1e7) ** 3) / (1e9), "Sf": 1e-2, "Sb": 1e-2}
+
+        # Iterations should proceed independent of which params are actively iterated,
+        # as all params are presumably needed to complete the simulation
+        param_info = {"names": param_names,
+                      "unit_conversions": unit_conversions,
+                      "active": {name: 0 for name in param_names}}
+        initial_guess = {"n0": 0,
+                         "p0": 0,
+                         "mu_n": 0,
+                         "mu_p": 0,
+                         "ks": 1e-20,
+                         "Sf": 0,
+                         "Sb": 0,
+                         "Cn": 0,
+                         "Cp": 0,
+                         "Tm": 300,
+                         "tauN": 1e99,
+                         "tauP": 1e99,
+                         "eps": 10}
+        param_info["init_guess"] = initial_guess
+
+        sim_flags = {"current_sigma": 1,
+                     "hmax": 4, "rtol": 1e-5, "atol": 1e-8,
+                     "self_normalize": ["TRPL"],
+                     "solver": "solveivp", }
+
+        p = Parameters(param_info)
+        p.apply_unit_conversions(param_info)
+        p2 = Parameters(param_info)
+        p2.apply_unit_conversions(param_info)
+
+        nt = 1000
+        running_hmax = [4] * len(iniPar)
+        times = [np.linspace(0, 100, nt+1), np.linspace(0, 100, nt+1)]
+        vals = [np.zeros(nt+1), np.zeros(nt+1)]
+        uncs = [np.ones(nt+1) * 1e-99, np.ones(nt+1) * 1e-99]
+        accepted = run_iteration(p, simPar, iniPar, times, vals, uncs, None,
+                                 running_hmax, sim_flags, verbose=True,
+                                 logger=self.logger, prev_p=None)
+
+        np.testing.assert_almost_equal(
+            p.likelihood, [0, 0], decimal=0)  # rtol=1e-5
+
 
     def test_one_sim_ll_errata(self):
         # TODO: The next time odeint fails to do a simulation, upload it into this
@@ -814,4 +876,4 @@ class TestUtils(unittest.TestCase):
 if __name__ == "__main__":
     t = TestUtils()
     t.setUp()
-    t.test_do_simulation()
+    t.test_run_iter_normalize()
