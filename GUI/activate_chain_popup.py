b37146c5e0e22db130122319bdfc45bdb5294d31
Calvin Fai <cfai2304@gmail.com>
2023-10-22 15:33:24 -0400

    Multicolumn chain popup

--- GUI/activate_chain_popup.py
+++ GUI/activate_chain_popup.py
@@ -8,23 +8,27 @@ import tkinter as tk
 from popup import Popup
 from gui_colors import LIGHT_GREY, PLOT_COLOR_CYCLE
 
-WIDTH = 280
+WIDTH_PER_COL = 100
+CHAINS_PER_ROW = 8
 BASE_HEIGHT = 30
-HEIGHT_PER_CHAIN = 40
+HEIGHT_PER_CHAIN = 18
 class ActivateChainPopup(Popup):
 
     def __init__(self, window, master) -> None:
         """Toggle the visibility of specific MCMC chains."""
-        super().__init__(window, master, WIDTH, BASE_HEIGHT + HEIGHT_PER_CHAIN * len(window.file_names))
+        n_cols = 1 + len(window.file_names) // CHAINS_PER_ROW
+        super().__init__(window, master,
+                         n_cols * WIDTH_PER_COL,
+                         BASE_HEIGHT + HEIGHT_PER_CHAIN * len(window.file_names))
         self.toplevel.attributes('-topmost', 'true')
         self.toplevel.title("Toggle MMC chains")
-        tk.Label(self.toplevel, text="Display:", background=LIGHT_GREY).grid(row=0, column=0, columnspan=2)
+        tk.Label(self.toplevel, text="Display:", background=LIGHT_GREY).grid(row=0, column=0, columnspan=99)
         for i, file_name in enumerate(self.window.file_names):
             tk.Checkbutton(self.toplevel, text=os.path.basename(file_name),
                            variable=self.window.file_names[file_name],
-                           onvalue=1, offvalue=0, background=LIGHT_GREY).grid(row=i+1, column=0)
+                           onvalue=1, offvalue=0, background=LIGHT_GREY).grid(row=i%CHAINS_PER_ROW+1, column=2*(i//CHAINS_PER_ROW))
             
-            tk.Label(self.toplevel, width=4, height=2, background=PLOT_COLOR_CYCLE[i % len(PLOT_COLOR_CYCLE)]).grid(row=i+1,column=1)
+            tk.Label(self.toplevel, width=4, height=2, background=PLOT_COLOR_CYCLE[i % len(PLOT_COLOR_CYCLE)]).grid(row=i%CHAINS_PER_ROW+1,column=1+2*(i//CHAINS_PER_ROW), padx=(0,5))
 
     def on_close(self) -> None:
         self.is_open = False
