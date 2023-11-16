db19360186518d9932ea0b8bcff66f4938e7c13c
Calvin Fai <cfai2304@gmail.com>
2023-11-16 15:08:54 -0500

    Add convolution

--- MLE/max_likelihood.py
+++ MLE/max_likelihood.py
@@ -3,6 +3,7 @@ import numpy as np
 from scipy.optimize import minimize
 
 from metropolis import do_simulation, search_c_grps
+from laplace import make_I_tables, do_irf_convolution, post_conv_trim
 from sim_utils import MetroState
 
 DEFAULT_NUM_ITERS = 1000
@@ -43,6 +44,8 @@ def cost(x, e_data, MS, logger):
 
     LOG_PL = MS.MCMC_fields["log_pl"]
     scale_f_info = MS.MCMC_fields.get("scale_factor", None)
+    irf_convolution = MS.MCMC_fields.get("irf_convolution", None)
+    IRF_tables = MS.MCMC_fields.get("IRF_tables", None)
     thicknesses = MS.sim_info["lengths"]
     nxes = MS.sim_info["nx"]
     for ic_num in range(MS.sim_info["num_meas"]):
@@ -55,11 +58,29 @@ def cost(x, e_data, MS, logger):
 
         tSteps, sol = do_simulation(MS.means, thickness, nx, MS.iniPar[ic_num], times, 1, meas=meas_type,
                                     solver=("solveivp",), model="std")
+        
+        if irf_convolution is not None and irf_convolution[ic_num] != 0:
+            wave = int(irf_convolution[ic_num])
+            tSteps, sol, success = do_irf_convolution(
+                tSteps, sol, IRF_tables[wave], time_max_shift=True)
+            if not success:
+                raise ValueError("Error: Interpolation for conv failed. Check measurement data"
+                                " times for floating-point inaccuracies.")
+            sol, times_c, vals_c, uncs_c = post_conv_trim(tSteps, sol, times, values, std)
+        else:
+            times_c = times
+            vals_c = values
+            uncs_c = std
+            sol = sol[-len(times_c):]
 
-        times_c = times
-        vals_c = values
-        uncs_c = std
-        sol = sol[-len(times_c):]
+        where_failed = sol < 0
+        n_fails = np.sum(where_failed)
+
+        if n_fails > 0 and logger is not None:
+            logger.warning(f"{MS.latest_iter}: {n_fails} / {len(sol)} non-positive vals")
+
+        sol[where_failed] *= -1
+
 
         if LOG_PL:
             sol = np.log10(sol)
@@ -105,6 +126,19 @@ def mle(e_data, sim_params, param_info, init_params, sim_flags, export_path, log
     logger.info(f"Sim info: {MS.sim_info}")
     logger.info(f"Param infos: {MS.param_info}")
     logger.info(f"MCMC fields: {MS.MCMC_fields}")
+
+    if MS.MCMC_fields.get("irf_convolution", None) is not None:
+        irfs = {}
+        for i in MS.MCMC_fields["irf_convolution"]:
+            if i > 0 and i not in irfs:
+                irfs[int(i)] = np.loadtxt(os.path.join("IRFs", f"irf_{int(i)}nm.csv"),
+                                            delimiter=",")
+
+        MS.MCMC_fields["IRF_tables"] = make_I_tables(irfs)
+        if logger is not None:
+            logger.info(f"Found IRFs for WLs {list(MS.MCMC_fields['IRF_tables'].keys())}")
+    else:
+        MS.MCMC_fields["IRF_tables"] = None
 
     # Optimize over only active params, while holding all others constant
     x0 = []
